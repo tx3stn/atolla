a198f9ef1e1a05002fa7358cf6f771184d2dc251
tx3stn <14163530+tx3stn@users.noreply.github.com>
2026-03-18 19:34:18 +0000

    fix: reset tab view by tapping on tab again

--- atolla/src/ui/views/HomeView.tsx
+++ atolla/src/ui/views/HomeView.tsx
@@ -12,6 +12,7 @@ export type HomeViewModel = Record<string, never>;
 
 interface HomeState {
 	activeTab: HeaderTab;
+	tabKeys: Record<HeaderTab, number>;
 }
 
 export class HomeView extends StatefulComponent<HomeViewModel, HomeState> {
@@ -19,20 +20,33 @@ export class HomeView extends StatefulComponent<HomeViewModel, HomeState> {
 
 	state: HomeState = {
 		activeTab: HeaderTabs.artists,
+		tabKeys: {
+			[HeaderTabs.artists]: 0,
+			[HeaderTabs.albums]: 0,
+			[HeaderTabs.playlists]: 0,
+		},
 	};
 
 	handleHeaderTabTap = (tab: HeaderTab): void => {
-		this.setState({ activeTab: tab });
+		if (tab === this.state.activeTab) {
+			this.setState({ tabKeys: { ...this.state.tabKeys, [tab]: this.state.tabKeys[tab] + 1 } });
+		} else {
+			this.setState({ activeTab: tab });
+		}
 	};
 
 	onRender(): void {
 		<view style={styles.root}>
 			<HomeHeaderNav activeTab={this.state.activeTab} onTabTap={this.handleHeaderTabTap} />
 
-			{this.state.activeTab === HeaderTabs.artists && <ArtistsView transport={this.transport} />}
-			{this.state.activeTab === HeaderTabs.albums && <AlbumsView transport={this.transport} />}
+			{this.state.activeTab === HeaderTabs.artists && (
+				<ArtistsView key={this.state.tabKeys[HeaderTabs.artists]} transport={this.transport} />
+			)}
+			{this.state.activeTab === HeaderTabs.albums && (
+				<AlbumsView key={this.state.tabKeys[HeaderTabs.albums]} transport={this.transport} />
+			)}
 			{this.state.activeTab === HeaderTabs.playlists && (
-				<PlaylistsView transport={this.transport} />
+				<PlaylistsView key={this.state.tabKeys[HeaderTabs.playlists]} transport={this.transport} />
 			)}
 		</view>;
 	}
