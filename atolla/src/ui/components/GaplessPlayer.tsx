89b49970c8cb96171b016360630482d949905773
tx3stn <14163530+tx3stn@users.noreply.github.com>
2026-05-01 22:00:52 +0100

    refactor: ios cleanup

--- atolla/src/ui/components/GaplessPlayer.tsx
+++ atolla/src/ui/components/GaplessPlayer.tsx
@@ -1,8 +1,6 @@
 import { Component } from 'valdi_core/src/Component';
-import { Device } from 'valdi_core/src/Device';
 import type { PlaybackStore } from '../../stores/Playback';
 import { NativeAudioPlayer } from './NativeAudioPlayer';
-import { VideoAudioPlayer } from './VideoAudioPlayer';
 
 export interface GaplessPlayerViewModel {
 	activeSourceUrl: string | null;
@@ -24,20 +22,7 @@ export class GaplessPlayer extends Component<GaplessPlayerViewModel> {
 			playbackStore,
 		} = this.viewModel;
 
-		if (Device.isAndroid()) {
-			<NativeAudioPlayer
-				isActive
-				nextPlaybackSourceUrl={nextSourceUrl}
-				onPlaybackError={onPlaybackError}
-				onPlaybackEvent={onPlaybackEvent}
-				onTrackCompleted={onTrackCompleted}
-				playbackSourceUrl={activeSourceUrl}
-				playbackStore={playbackStore}
-			/>;
-			return;
-		}
-
-		<VideoAudioPlayer
+		<NativeAudioPlayer
 			isActive
 			nextPlaybackSourceUrl={nextSourceUrl}
 			onPlaybackError={onPlaybackError}
